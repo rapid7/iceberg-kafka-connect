eca9eac9b594258251d43bef23e43e6d5600b47b
gmcgoldrick-r7 <glenn_mcgoldrick@rapid7.com>
2024-09-25 14:35:02 +0100

    change us to ms

--- kafka-connect-transforms/src/main/java/io/tabular/iceberg/connect/transforms/DebeziumTransform.java
+++ kafka-connect-transforms/src/main/java/io/tabular/iceberg/connect/transforms/DebeziumTransform.java
@@ -110,8 +110,8 @@ public class DebeziumTransform<R extends ConnectRecord<R>> implements Transforma
     }
     newValue.put(CdcConstants.COL_CDC, cdcMetadata);
 
-    if (value.getStruct("ts_us") != null) {
-      newValue.put(CustomFieldConstants.SOURCE_TIMESTAMP_US, new java.util.Date(value.getInt64("ts_us")));
+    if (value.getStruct("ts_ms") != null) {
+      newValue.put(CustomFieldConstants.SOURCE_TIMESTAMP_MS, new java.util.Date(value.getInt64("ts_ms")));
     }
 
     return record.newRecord(
@@ -159,8 +159,8 @@ public class DebeziumTransform<R extends ConnectRecord<R>> implements Transforma
     Map<String, Object> newValue = Maps.newHashMap((Map<String, Object>) payload);
     newValue.put(CdcConstants.COL_CDC, cdcMetadata);
 
-    if (value.containsKey("ts_us")) {
-      newValue.put(CustomFieldConstants.SOURCE_TIMESTAMP_US, value.get("ts_us"));
+    if (value.containsKey("ts_ms")) {
+      newValue.put(CustomFieldConstants.SOURCE_TIMESTAMP_MS, value.get("ts_ms"));
     }
 
     return record.newRecord(
